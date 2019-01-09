1feb0065863ebaa302c69cc75b685b3d04e0a9b2
gzx <493004728@qq.com>
2019-01-09 15:31:07 +0800

    Initial commit

---
+++ src/main/java/com/owalai/eva0/Eva0Application.java
@@ -0,0 +1,14 @@
+package com.owalai.eva0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eva0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eva0Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/owalai/eva0/Eva0ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.owalai.eva0;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Eva0ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
